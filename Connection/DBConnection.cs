100a95e29c22a3e3ec52a66eb12f7161229246f9
4e6yPaLIIKa228 <alexander_pushkar@mail.ru>
2021-12-19 17:40:54 +0300

    test
    test

---
+++ Connection/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Doctors.Connection
+{
+    class DBConnection
+    {
+        public static string myConn = $@"Data Source=DoctorYhet.db;Version=3;";
+    }
+}
